f60836ab0f797f0b2d0209ca3757ff72a8a6df6b
vithorDeveloper <vitorsantosdev663@gmail.com>
2023-12-01 17:23:54 -0300

    first commit

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 $(document).ready(function(){
 
   $('#telefone').mask("(00) 00000-0000");
-  $('#cpf').mask("000.000.000.00");
-  $('#cep').mask("00000-000");
 
   $('form').validate({
 
     rules: {
       telefone: {required: true},
-      cpf: {required: true},
-      cep: {required: true},
     },
 
     messages: {
       telefone: "o campo Telefone é obrigatório",
-      cpf: "o campo CPF é obrigatório",
-      cep: "o campo CEP é obrigatório",
     },
 
     submitHandler: (function(form){
@@ -34,11 +28,5 @@ $(document).ready(function(){
     })
 
   });
-
-  $('#carrossel').slick({
-    autoplay: true,
-    fade:true,
-    speed: 4000,
-  })
 
 })
