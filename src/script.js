f01cbe0d93c211ef7e74eec7781ee3b6a246c6d6
vithorDeveloper <vitorsantosdev663@gmail.com>
2024-01-12 15:12:59 -0300

    first commit

--- src/script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll(".product-button")
-
-  buttons.forEach((button) => {
-    button.addEventListener("click", function(){
-      alert("Produto esgotado!");
-    })
-  })
