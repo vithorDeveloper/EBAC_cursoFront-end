352c4c336778fbf09fcdcd6ca772618f1bbf61c0
vithorDeveloper <vitorsantosdev663@gmail.com>
2024-02-08 14:57:20 -0300

    first commit

--- script.js
+++ script.js
@@ -1,55 +1,34 @@
+const alunos = [
+  {
+    nome: "vitor",
+    nota: 1
+  },
+  {
+    nome: "vitoria",
+    nota: 6
+  },
+  {
+    nome: "maria",
+    nota: 7
+  },
+  {
+    nome: "pedro",
+    nota: 4
+  },
+  {
+    nome: "paulo",
+    nota: 5
+  },
+]
 
-// Usando Classe
-class Carro{
-  constructor(modelo, cor, preco){
-    this.modelo = modelo;
-    this.cor = cor;
-    this.preco = preco;
+const notaSeis = (arrayDeAlunos) => {
+
+  for (let item of arrayDeAlunos) {
+
+    if(item.nota >= 6){
+      console.log(item.nome)
+    }
   }
-}
+} 
 
-class CarroZeroKm extends Carro {
-  constructor(modelo, cor, preco, zeroKm){
-    super(modelo, cor, preco)
-      this.zeroKm = zeroKm
-  }
-}
-
-class CarroUsado extends Carro {
-  constructor(modelo, cor, preco, usado){
-    super(modelo, cor, preco)
-    this.usado = usado
-  }
-}
-
-
-
-const volvo = new CarroZeroKm("VC60", "preto", 170000, true)
-const rangeRover = new CarroZeroKm("Velar", "prata", 150000, true)
-const nissan = new CarroUsado("Kicks", "branco", 70000, true)
-
-console.log(volvo, rangeRover, nissan)
-
-
-// Usando Função construtora
-function Moto (modelo, cor, preco){
-  this.modelo = modelo
-  this.cor = cor
-  this.preco = preco
-}
-
-function MotoZeroKm (modelo, cor, preco, zeroKm){
-  Moto.call(this, modelo, cor, preco)
-  this.zeroKm = zeroKm
-}
-
-function MotoUsada (modelo, cor, preco, usada){
-  Moto.call(this, modelo, cor, preco)
-  this.usada = usada
-}
-
-const honda = new MotoZeroKm("Titan 160", "vermelha", 18000, true)
-const yamaha = new MotoUsada("Fazer 150", "Azul", 11000, true)
-const shineray = new MotoZeroKm("SHE-S", "preta", 10000, true)
-
-console.log(honda, yamaha, shineray)
+notaSeis(alunos)
