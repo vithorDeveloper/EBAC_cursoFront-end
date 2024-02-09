666fd85f43331b5e47fe32038ff6804672148926
vithorDeveloper <vitorsantosdev663@gmail.com>
2024-02-09 14:28:02 -0300

    first commit

---
+++ src/funcoes.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplicar = (a, b) => { return a * b; };
+console.log(multiplicar(2, 2));
+const saudação = (nome) => {
+    console.log(`Olá ${nome}`);
+};
+saudação('vitor');
---
+++ src/funcoes.ts
@@ -0,0 +1,9 @@
+const multiplicar = (a: number, b: number) : number => {return a * b};
+
+console.log(multiplicar(2, 2));
+
+const saudação = (nome: string) : void => {
+  console.log(`Olá ${nome}`)
+}
+
+saudação('vitor')
